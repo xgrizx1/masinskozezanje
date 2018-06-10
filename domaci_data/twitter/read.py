d1bc7d71c1ee76a825733358fbe4e5c0c74f138c
aleksandar.ogrizovic@symphony.is <aleksandar.ogrizovic@symphony.is>
2018-06-10 18:47:35 +0200

    something

---
+++ domaci_data/twitter/read.py
@@ -0,0 +1,18 @@
+import csv
+
+def load_dateset():
+    X, y = [], []
+    with open('train.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        next(reader, None) # Skip header
+        for row in reader:
+            y.append(int(row[1]))
+            X.append(row[2])
+    return X, y
+
+
+if __name__ == '__main__':
+    X, y = load_dateset()
+
+    for i in range(4):
+        print(X[i], y[i], type(X[i]), type(y[i]))
